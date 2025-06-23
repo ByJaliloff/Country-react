790fb975c320f43bd6c7d36039f450506ecb5dc0
Orxan Celilov <orxan.celilov18@gmail.com>
2025-06-23 17:13:42 +0400

    yeni

--- src/service/CountryService.js
+++ src/service/CountryService.js
@@ -1,18 +1,16 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log("BASE_URL:", BASE_URL); 
 
-
-async function getAllCountries () {
-    try {
-        const res = await fetch(BASE_URL)
-        if(!res.ok) {
-            throw new Error ('fetch emeliyyatinda xeta bas verdi');
-        }
-        const data = await res.json()
-        return data
-    }
-    catch (err) {
-        console.error(err.message)
-    }
+async function getAllCountries() {
+  try {
+    const res = await fetch(BASE_URL);
+    if (!res.ok) throw new Error("fetch emeliyyatinda xeta bas verdi");
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error("Xəta:", err.message);
+    return [];
+  }
 }
 
-export {getAllCountries}
+export { getAllCountries };
